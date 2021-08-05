e6edb53eb7d25e2a3c718e4bcf484f290f251249
Abdullah Meda <am2024@hw.ac.uk>
2021-08-05 14:23:47 +0400

    Adding FootNotes for NIO.2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import java.io.IOException;
+import java.nio.file.Paths;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
+        System.out.println(Paths.get("."));
     }
 
 }
