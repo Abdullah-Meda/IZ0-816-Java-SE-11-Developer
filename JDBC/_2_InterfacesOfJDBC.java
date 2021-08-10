67d9fec70fdac09e5a8fc990e8100ae6c581ef11
Abdullah Meda <am2024@hw.ac.uk>
2021-08-10 17:32:44 +0400

    Minor edits and adding footnotes

--- JDBC/_2_InterfacesOfJDBC.java
+++ JDBC/_2_InterfacesOfJDBC.java
@@ -3,11 +3,11 @@ import java.sql.*;
 public class _2_InterfacesOfJDBC {
 
     /* Key JDBC Interfaces:
-            - Driver: Establishes a connection to the database
-            - Connection: Sends commands to a database
-            - PreparedStatement: Executes a SQL query
-            - CallableStatement: Executes commands stored in the database
-            - ResultSet: Reads results of a query
+            - Driver:               Establishes a connection to the database
+            - Connection:           Sends commands to a database
+            - PreparedStatement:    Executes a SQL query
+            - CallableStatement:    Executes commands stored in the database
+            - ResultSet:            Reads results of a query
      */
 
     public static void main(String[] args) throws SQLException {
